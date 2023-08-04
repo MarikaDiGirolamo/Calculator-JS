0d0d41b7aee582989f551715a3b50749f1d7e207
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-08-04 10:18:20 +0200

    Add class js and query selction

---
+++ main.js
@@ -0,0 +1,5 @@
+//Query for all numeric buttons
+const numBtn = document.querySelectorAll(".numBtn");
+const btnAcAll = document.querySelectorAll(".btnAcAll");
+const btnDel = document.querySelectorAll(".btnDel");
+const btnOperator = document.querySelectorAll(".btnOperator");
